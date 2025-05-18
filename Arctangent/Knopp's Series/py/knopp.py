cb51232a29a85c201e9dd932dfe3e4366ab4ef3d
Dhruv <dhruvmitna9@gmail.com>
2025-05-18 13:45:59 +0530

    Tkinter integration groundwork

--- Arctangent/Knopp's Series/py/knopp.py
+++ Arctangent/Knopp's Series/py/knopp.py
@@ -1,16 +1,17 @@
-#TODO: deviation, tkinter integration
+#TODO: deviation
 
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from math import atan as arctan
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter as tk
 
 figure, axes = plt.subplots()
 x: list[float] = []
 y: list[float] = []
-placeholder_name, = axes.plot(x, y)
+graph, = axes.plot(x, y)
 
-def artists(): return placeholder_name # fixes the repitition of the ordered pair (0, y).
+def artists(): return graph # fixes the repitition of the ordered pair (0, y).
 
 def next_value(k: int) -> float:
     if k == 1: return 4 * arctan(1/3)
@@ -20,15 +21,22 @@ def update_graph(frame):
 
     x.append(frame)
     y.append(next_value(frame + 1))
-    placeholder_name.set_data(x, y)
+    graph.set_data(x, y)
 
     figure.gca().relim()
     figure.gca().autoscale_view()
 
 plt.axhline(y=3.141592653589793238462643383279502884197169399375, color='r', linestyle='--', label="pi") # Adds a horizontal reference line at y = pi.
 
-animation = FuncAnimation(figure, func=update_graph, interval=0, init_func=artists, repeat=False)
-placeholder_name.set_label("approximation")
+root = tk.Tk()
+frame = tk.Frame(root)
+canvas = FigureCanvasTkAgg(figure=figure, master=frame)
+canvas.get_tk_widget().pack()
 
+animation = FuncAnimation(figure, func=update_graph, interval=0, init_func=artists, repeat=False)
+
+graph.set_label("approximation")
 plt.legend()
-plt.show()
+
+frame.pack()
+root.mainloop()
