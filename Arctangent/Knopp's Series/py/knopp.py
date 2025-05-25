6bc9a97ae789d996a8d02bd53fb26608b6713771
Dhruv <dhruvmitna9@gmail.com>
2025-05-25 15:54:28 +0530

    ...

--- Arctangent/Knopp's Series/py/knopp.py
+++ Arctangent/Knopp's Series/py/knopp.py
@@ -4,15 +4,20 @@ from math import atan as arctan
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter as tk
 from tkinter import scrolledtext
-from time import sleep
 
 pi = 3.141592653589793238462643383279502884197169399375 # accurate value used for error calculation.
 
 figure, axes = plt.subplots()
-x: list[float] = []
+x: list[int] = []
 y: list[float] = []
 graph, = axes.plot(x, y)
 
+###### local_figure, axes_local = plt.subplots()
+###### x_local: list[int] = []
+###### y_local: list[float] = []
+###### graph_local, = axes_local.plot(x_local, y_local)
+
+######def artists(): return graph, graph_local # fixes the repitition of the ordered pair (0, y).
 def artists(): return graph # fixes the repitition of the ordered pair (0, y).
 
 def approximation_function(k: int) -> float:
@@ -55,11 +60,10 @@ deviation = {deviation}
 error_percentage = {(y[-1] - pi) * 100 / pi}%
 """)
 
-    # The current iteration info text keeps flickering and sometimes even disappearing for some time because iterations go by too fast. I overlap the 2 latest iteration info labels (newer one on top) and delete all previous ones to minimize the flickering while also not overloading the label frame with thousands of labels corresponding to each iteration.
+    # The current iteration info text keeps flickering, even disappearing for some time because iterations go by too fast. I overlap the 2 latest iteration info labels (newer one on top) and delete all previous ones to minimize the flickering while also not overloading the label frame with thousands of labels corresponding to each iteration.
     current_iteration_info.place(x=20, y=20)
     for widget in current_iteration_info_frame.winfo_children():
         if len(current_iteration_info_frame.winfo_children()) > 2: widget.destroy()
-
 
 def update_graph(frame):
     x.append(frame)
@@ -67,6 +71,22 @@ def update_graph(frame):
     graph.set_data(x, y)
     figure.gca().relim()
     figure.gca().autoscale_view()
+
+    ###### if len(x) < 5:
+    ######     x_local, y_local = x, y
+    ###### else: x_local, y_local = x[-5:], y[-5:]
+
+    ###### # x_local.append(frame)
+    ###### # y_local.append(approximation_function(frame + 1))
+    ###### # if len(x_local) > 5:
+    ###### #     x_local.pop(0)
+    ###### #     y_local.pop(0)
+
+    ###### axes_local.clear()
+    ###### graph_local.set_data(x_local, y_local)
+    ###### local_figure.gca().relim()
+    ###### local_figure.gca().autoscale_view()
+
 
 def update_all_widgets(frame):
 
@@ -82,11 +102,16 @@ plot_frame = tk.Frame(root)
 canvas = FigureCanvasTkAgg(figure=figure, master=plot_frame)
 canvas.get_tk_widget().pack()
 
+##########local_plot_frame = tk.Frame(master=root)
+##########canvas_local = FigureCanvasTkAgg(figure=figure_local, master=local_plot_frame)
+##########canvas_local.get_tk_widget().pack()
+
 animation = FuncAnimation(figure, func=update_all_widgets, interval=0, init_func=artists, repeat=False)
 
 graph.set_label("approximation")
 plt.legend()
 
 plot_frame.place(x=0, y=0)
+##########local_plot_frame.pack(side=tk.RIGHT)
 
 root.mainloop()
